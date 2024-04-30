64969881db8f524c23c12284dc7b491944bc890f
Sabbir Hossen <sabbir.ms2000@gmail.com>
2024-04-30 11:28:18 +0600

    send to data from database to ui

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
@@ -7,14 +8,12 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-// paper_craft
-// 1lmpquc04ZW6fMhb
-
 
 // mongodb connected
 
-// const uri = "mongodb+srv://<username>:<password>@cluster0.neggqyg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const uri = "mongodb+srv://paper_craft:1lmpquc04ZW6fMhb@cluster0.neggqyg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.neggqyg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,7 +28,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+    const papersCollection = client.db('papersDB').collection('papers')
+
+    app.get('/papers', async (req, res) => {
+      const cursor = papersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.post('/papers', async (req, res) => {
+      const newPaper = req.body;
+      console.log(newPaper);
+      const result = await papersCollection.insertOne(newPaper)
+      res.send(result)
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
